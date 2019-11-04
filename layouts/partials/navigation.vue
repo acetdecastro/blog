9465dcd560da927362ab463ee093bdbc2a60e9e7
acetdecastro <acetdecastro@gmail.com>
2019-11-04 11:20:30 +0800

    :star: Finish Authentication

--- layouts/partials/navigation.vue
+++ layouts/partials/navigation.vue
@@ -1,22 +1,22 @@
 <template>
-  <header class="bg-gray-100 shadow-lg">
+  <header class="bg-blue-900 shadow-xl">
     <div class="container py-2 px-6 mx-auto">
       <nav class="flex justify-between items-center">
-        <nuxt-link to="/" class="text-3xl font-bold tracking-wide">Blog</nuxt-link>
+        <nuxt-link to="/" class="text-3xl text-gray-200 font-bold tracking-wide">Blog</nuxt-link>
 
         <div
           v-if="! authenticated"
-          class="text-gray-700 font-semibold"
+          class="text-gray-200 font-semibold"
         >
           <nuxt-link 
             to="/login"
-            class="mr-3 hover:text-gray-800 hover:text-gray-900"
+            class="mr-3 hover:text-teal-300 hover:text-gray-900"
           >
             Log In
           </nuxt-link>
           <nuxt-link
             to="/register"
-            class="py-2 px-3 text-gray-800 rounded-lg bg-teal-300 hover:bg-teal-200 focus:bg-teal-200"
+            class="py-2 px-3 text-gray-900 rounded-lg bg-teal-300 hover:bg-teal-200 focus:bg-teal-200"
           >
             Sign Up
           </nuxt-link>
@@ -27,7 +27,7 @@
         >
           <button
             @click="logout"
-            class="text-gray-700 hover:text-gray-800 hover:text-gray-900 focus:outline-none"
+            class="text-gray-900 hover:text-gray-800 hover:text-gray-900 focus:outline-none"
           >
             Log Out
           </button>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,37 @@
 <template>
   <div>
-    
+    <login />
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import login from "./login";
 
 export default {
-  components: {
-    Logo
-  }
+  components : {
+    login
+  },
+
+  // data () {
+  //   return {
+  //     form : {
+  //       title : 'Title Sample',
+  //       description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo magni itaque enim reiciendis labore vero sint dolor, ut quis at quo. Ipsum dolorum labore nulla tempora fuga placeat, optio suscipit.'
+  //     }
+  //   }
+  // },
+
+  // created () {
+  //   this.$axios
+  //       .post("/articles", this.form)
+  //       .then(response => {
+  //         console.log(response.data)
+  //       })
+  //       .catch(errors => {
+  //         console.log(errors.response.data.errors);
+  //       });
+
+  // }
 }
 </script>
 
